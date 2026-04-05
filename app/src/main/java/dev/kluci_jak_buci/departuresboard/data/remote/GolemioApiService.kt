05f27b3a8ad228fda97f306a77d9e260b43fe3a4
Jindřich Šilhan <jindrich@silhan.dev>
2026-04-05 20:50:44 +0200

    Remove old singleton pattern from GolemioApiService

--- app/src/main/java/dev/kluci_jak_buci/departuresboard/data/remote/GolemioApiService.kt
+++ app/src/main/java/dev/kluci_jak_buci/departuresboard/data/remote/GolemioApiService.kt
@@ -1,62 +1,9 @@
 package dev.kluci_jak_buci.departuresboard.data.remote
 
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonDeserializer
 import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import kotlin.time.Instant
-import dev.kluci_jak_buci.departuresboard.BuildConfig.GOLEMIO_API_KEY
-
-private const val BASE_URL = "https://api.golemio.cz"
-
-val instantDeserializer= JsonDeserializer { json, _, _ ->
-    json?.asString?.let { Instant.parse(it) }
-}
-
-private val httpClient = OkHttpClient.Builder()
-    // Add API KEY to every request
-    .addInterceptor { chain ->
-        val modifiedRequest = chain.request().newBuilder()
-            .header("X-Access-Token", GOLEMIO_API_KEY)
-            .build()
-        chain.proceed(modifiedRequest)
-    }
-    // Convert Station ID to JSON structure
-    .addInterceptor { chain ->
-        val request = chain.request()
-
-        val stopIds = request.url.queryParameterValues("json_stopId")
-        if(stopIds.isNotEmpty()) {
-            val stopIdsJson = """{"0": ${Json.encodeToString(stopIds)}}"""
-
-            val modifierUrl = request.url.newBuilder()
-                .removeAllQueryParameters("json_stopId")
-                .addQueryParameter("stopIds", stopIdsJson)
-                .build()
-            val modifiedRequest = request.newBuilder()
-                .url(modifierUrl)
-                .build()
-            chain.proceed(modifiedRequest)
-        } else {
-            chain.proceed(request)
-        }
-    }
-    .build()
-
-private val gson = GsonBuilder()
-    .registerTypeAdapter(Instant::class.java, instantDeserializer)
-    .create()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create(gson))
-    .client(httpClient)
-    .baseUrl(BASE_URL)
-    .build()
 
 interface GolemioApiService {
     @GET("v2/public/departureboards")
@@ -67,12 +14,6 @@ interface GolemioApiService {
         @Query("minutesAfter") minutesAfter: Number = 60,
         @Query("minutesBefore") minutesBefore: Number = 0,
     ): List<List<DepartureGroup>>
-}
-
-object GolemioApi {
-    val retrofitService: GolemioApiService by lazy {
-        retrofit.create(GolemioApiService::class.java)
-    }
 }
 
 data class DepartureGroup(
